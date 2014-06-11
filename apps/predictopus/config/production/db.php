6cb4cde1be72370d8283820d541072fad6e24c65
asao <anshul.sao@gmail.com>
2014-06-11 15:08:25 +0530

    New db

--- apps/predictopus/config/production/db.php
+++ apps/predictopus/config/production/db.php
@@ -1,14 +1,42 @@
 <?php
-/**
- * The production database settings. These get merged with the global settings.
- */
 
+/**
+ * Use this file to override global defaults.
+ *
+ * See the individual environment DB configs for specific config information.
+ */
 return array(
-	'default' => array(
-		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
-		),
-	),
+    'active' => 'local',
+    'local' => array(
+        'type' => 'mysqli',
+        'connection' => array(
+            'hostname' => 'predictopusrds.css2pwnegpst.us-east-1.rds.amazonaws.com',
+            'port' => '3306',
+            'database' => 'predictopus',
+            'username' => 'predictopus',
+            'password' => 'Predict0pus',
+            'persistent' => true,
+            'compress' => true,
+        ),
+        'identifier' => '',
+        'table_prefix' => '',
+        'charset' => 'utf8',
+        'enable_cache' => true,
+        'profiling' => false,
+        'readonly' => false,
+    ),
+    'lite' => array(
+        'type' => 'pdo',
+        'connection' => array(
+            'dsn' => 'sqlite:'.ROOT.'football.db',
+            'username' => '',
+            'password' => '',
+        ),
+        'identifier' => '',
+        'table_prefix' => '',
+        'charset' => 'utf8',
+        'enable_cache' => true,
+        'profiling' => true,
+        'readonly' => false,
+    )
 );
