e8250188c57fb954942cb88b17731c09090a9bdd
asao <anshul.sao@gmail.com>
2014-06-13 01:46:50 +0530

    Show Points

--- apps/predictopus/config/production/config.php
+++ apps/predictopus/config/production/config.php
@@ -12,7 +12,7 @@
  */
 return array(
     'cdn' => '//static.playpredictopus.com',
-    'cdnversion' => 'prod-000004',
+    'cdnversion' => 'prod-000005',
     'profiling' => false,
     'disqus_params' => array(
         'forum' => 'predictopus',
