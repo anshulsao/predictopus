831ae065ada0f6f841ac7648adcef3d87a6d0f11
asao <anshul.sao@gmail.com>
2014-06-12 20:30:51 +0530

    Mobile PredictNow

--- apps/predictopus/config/production/config.php
+++ apps/predictopus/config/production/config.php
@@ -12,7 +12,7 @@
  */
 return array(
     'cdn' => '//static.playpredictopus.com',
-    'cdnversion' => 'prod-000002',
+    'cdnversion' => 'prod-000003',
     'profiling' => false,
     'disqus_params' => array(
         'forum' => 'predictopus',
