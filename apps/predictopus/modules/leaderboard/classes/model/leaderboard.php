f586646b0201d12a95b25b63d01a8d8fed835468
asao <anshul.sao@gmail.com>
2014-06-15 01:21:28 +0530

    leaderboard

--- apps/predictopus/modules/leaderboard/classes/model/leaderboard.php
+++ apps/predictopus/modules/leaderboard/classes/model/leaderboard.php
@@ -18,7 +18,7 @@ class Model_LeaderBoard extends \Model_Base {
     public static function getLeaderBoard($limit = 20) {
         try {
             
-            $query = \Fuel\Core\DB::query("select c_rel_users_league.user_id, rank, value, a.points from c_rel_users_league left join (select parent_id,points,value from users_metadata left join c_users_scores on users_metadata.parent_id=c_users_scores.user_id where `key`='nickname') a on a.parent_id = c_rel_users_league.user_id where rank >0 order by rank asc limit $limit");
+            $query = \Fuel\Core\DB::query("select c_rel_users_league.user_id, rank, value, a.points from c_rel_users_league left join (select parent_id,points,value from users_metadata left join c_users_scores on users_metadata.parent_id=c_users_scores.user_id where `key`='fullname') a on a.parent_id = c_rel_users_league.user_id where rank >0 order by rank asc limit $limit");
             $leader = $query->execute(self::DB_NAME)->as_array();
             return $leader;
         } catch (Exception $e) {
