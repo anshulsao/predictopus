ec9e55f07e21a123b8b97d8f42e5ee4171ac2739
asao <anshul.sao@gmail.com>
2014-06-15 23:42:14 +0530

    Twitter login

--- apps/predictopus/config/production/config.php
+++ apps/predictopus/config/production/config.php
@@ -12,7 +12,7 @@
  */
 return array(
     'cdn' => '//static.playpredictopus.com',
-    'cdnversion' => 'prod-000006',
+    'cdnversion' => 'prod-000007',
     'profiling' => false,
     'disqus_params' => array(
         'forum' => 'predictopus',
