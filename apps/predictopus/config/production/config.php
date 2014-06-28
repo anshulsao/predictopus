aac14f188471a1766b4bfe8b1bf91755b970cd51
asao <anshul.sao@gmail.com>
2014-06-28 18:44:09 +0530

    Leader Board Fix

--- apps/predictopus/config/production/config.php
+++ apps/predictopus/config/production/config.php
@@ -12,7 +12,7 @@
  */
 return array(
     'cdn' => '//static.playpredictopus.com',
-    'cdnversion' => 'prod-000009',
+    'cdnversion' => 'prod-000010',
     'profiling' => false,
     'activeLeague' => 1,
     'disqus_params' => array(
