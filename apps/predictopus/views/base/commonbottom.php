34aa6055bb5f2d7eac9a21dd6b8569cc5a03a17d
asao <anshul.sao@gmail.com>
2014-06-29 21:01:03 +0530

    Veho ad

--- apps/predictopus/views/base/commonbottom.php
+++ apps/predictopus/views/base/commonbottom.php
@@ -24,14 +24,17 @@
 <script id="stickyjs">
     YCustom.use('node', 'services', function(Y) {
         Y.later(1000, this, function() {
-            if (Y.one("#right")) {
-                new Y.services.StickyService({
-                    node: Y.one("#right>div"),
-                    stickPosition: "bottom",
-                    offsetWidth: 30
-                });
-
-            }            
+            try {
+                if (Y.one("#right")) {
+                    new Y.services.StickyService({
+                        node: Y.one("#right>div"),
+                        stickPosition: "bottom",
+                        offsetWidth: 30
+                    });
+                }
+            } catch (e) {
+                console.log('Sticky Service failed');
+            }
         });
     });
 
