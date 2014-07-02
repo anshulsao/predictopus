fcd8f4ff377f1a99f25a65155dc903e0c4050633
asao <anshul.sao@gmail.com>
2014-07-02 21:11:48 +0530

    Program list overlap issue

--- apps/predictopus/modules/ads/classes/controller/ads.php
+++ apps/predictopus/modules/ads/classes/controller/ads.php
@@ -15,15 +15,29 @@ class Controller_Ads extends \Controller_ModuleBase {
         );
         return $this->render($data);
     }
+
     public function action_lrec() {
+        $modData = array(
+            'referral' => 'http://veho.in/?referrer=playpredictopus',
+            'adImage' => 'http://static.playpredictopus.com/assets/img/veho.jpg'
+        );
+        $random = rand(1, 10);
+        if ($random <= 3) {
+            $modData = array(
+                'referral' => 'http://www.postergully.com/?referrer=playpredictopus',
+                'adImage' => 'http://static.playpredictopus.com/assets/img/postergully.jpg'
+            );
+        }
+
         $data = array(
             "moduleId" => 'lrec',
             "moduleClasses" => '',
-            "content" => \View::forge('lrec.mustache', array()),
+            "content" => \View::forge('lrec.mustache', $modData),
             "js" => array(),
             "css" => array(),
             "inlineJs" => '',
         );
         return $this->render($data);
     }
+
 }
